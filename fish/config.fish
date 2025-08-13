8612288ce189e35ffbf57b84d94d4bdd6e42d907
tiioxic <toto27160@proton.me>
2025-08-13 22:42:20 +0200

    [INIT] My custom rice caelestia

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,37 @@
 if status is-interactive
+    set -gx LANG fr_FR.UTF-8
+
+    # Start SSH
+    # eval (ssh-agent -s)
+
     # Starship custom prompt
+    starship preset gruvbox-rainbow -o ~/.config/starship.toml
     starship init fish | source
 
     # Custom colours
     cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    export PATH="$HOME/.local/bin:$PATH"
+    #alias docker-desktop='/opt/docker-desktop/bin/docker-desktop'
 
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
         echo -en "\e]133;A\e\\"
+    end
+
+    function caelestia-logs
+        cat /tmp/caelestia-pull.log
+    end
+
+    function caelestia-update
+        git -C ~/.local/share/caelestia pull &>> /tmp/caelestia-pull.log && cat /tmp/caelestia-pull.log
+    end
+
+    function caelestia-config  
+        codium ~/.local/share/caelestia
+    end
+
+    function clear
+        command clear
+        pokefetch
     end
 end
